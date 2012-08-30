f3b96f0c85ea0006d2b65b029828288e7192da2e
Andrew Gertig <andrew.gertig@gmail.com>
2012-08-30 17:35:36 -0300

    Update lib/auto_html/filters/simple_format.rb
    oops

--- lib/auto_html/filters/simple_format.rb
+++ lib/auto_html/filters/simple_format.rb
@@ -2,7 +2,7 @@ require 'action_view'
 
 AutoHtml.add_filter(:simple_format).with(:class => "auto-html") do |text, html_options|
   css_class = html_options[:class]
-  args = [text, :class => css_class, {:sanitize => false}]
+  args = [text, {:class => css_class}, {:sanitize => false}]
   begin
     ActionView::Base.new.simple_format(*args) 
   rescue ArgumentError
