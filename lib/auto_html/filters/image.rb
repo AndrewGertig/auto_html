694291fe25ef8c4b6d56a19f728f2ac7df0bc3a8
AndrewGertig <andrew.gertig@gmail.com>
2012-08-30 23:33:11 -0400

    don't link images

--- lib/auto_html/filters/image.rb
+++ lib/auto_html/filters/image.rb
@@ -12,8 +12,8 @@ AutoHtml.add_filter(:image).with(:alt => 'default-alt', :class => '') do |text, 
   klass = options[:class]
   puts "[APG] image.rb ********************************"
   img = text.gsub(/https?:\/\/.+?\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
-    r.render("[![#{alt}](#{match})](#{match}))")
-    #r.render("![#{alt}](#{match})")
+    # r.render("[![#{alt}](#{match})](#{match}))")
+    r.render("![#{alt}](#{match})")
   end
   "<div class='#{klass}'>" + img + "</div>"
 end
