00834798466fffa11b05dff0375a4997df28e77d
AndrewGertig <andrew.gertig@gmail.com>
2012-08-30 23:58:12 -0400

    remove the div wrapper on images

--- lib/auto_html/filters/image.rb
+++ lib/auto_html/filters/image.rb
@@ -10,10 +10,10 @@ AutoHtml.add_filter(:image).with(:alt => 'default-alt', :class => '') do |text, 
   r = Redcarpet::Markdown.new(NoParagraphRenderer)
   alt = options[:alt]
   klass = options[:class]
-  puts "[APG] image.rb ********************************"
+  # puts "[APG] image.rb ********************************"
   img = text.gsub(/https?:\/\/.+?\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
     # r.render("[![#{alt}](#{match})](#{match}))")
     r.render("![#{alt}](#{match})")
   end
-  "<div class='#{klass}'>" + img + "</div>"
+  # "<div class='#{klass}'>" + img + "</div>"
 end
